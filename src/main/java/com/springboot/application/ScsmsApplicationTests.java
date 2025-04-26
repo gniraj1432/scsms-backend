2ee22b4f5cdf6e766eaeb943101f4c44891c5276
gniraj1432 <gniraj1432@gmail.com>
2025-04-27 01:43:52 +0530

    Test Class Added with JAR/WAR files

---
+++ src/main/java/com/springboot/application/ScsmsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springboot.application;
+
+import com.springboot.application.ScsmsApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ScsmsApplication.class) // Fix: link to actual main class
+class ScsmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
