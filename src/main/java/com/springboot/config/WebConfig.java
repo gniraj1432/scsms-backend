362715fe6e6d4d5c2c92db416372dbe43c20f2dc
gniraj1432 <gniraj1432@gmail.com>
2025-04-28 23:17:06 +0530

    WebConfig.java file updated for Context-Path

--- src/main/java/com/springboot/config/WebConfig.java
+++ src/main/java/com/springboot/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/scsms-1.0.0/**")
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000", "https://scsms.vercel.app") // React application URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
